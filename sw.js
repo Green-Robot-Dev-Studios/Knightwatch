3ab673336f7bc582e978dda00402a56d5e15691f
Green-Robot-Dev-Studios <Green-Robot-Dev-Studios@users.noreply.github.com>
2021-07-01 20:05:49 +0000

    deploy: ac02fc381d2e5942e06f9cb90bf84a282a6d4dea

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches
+      .open("kw-store")
+      .then((cache) =>
+        cache.addAll([
+          "./index.html"
+        ])
+      )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
