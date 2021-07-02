18ddf8ba54aee987df8ded8acbdb5a89f7bcc371
Green-Robot-Dev-Studios <nficara01@gmail.com>
2021-07-02 11:42:59 -0400

    Added condition to CD

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 [![Convert and Push to Github Pages](https://github.com/Green-Robot-Dev-Studios/Knightwatch/actions/workflows/deploy.yml/badge.svg)](https://github.com/Green-Robot-Dev-Studios/Knightwatch/actions/workflows/deploy.yml)
 
 # A school newspaper
+
+# TODO
+- Tags list on main page
+- Image scaling
+- CSS
+- GitHub online zip file addition
+- A2HS
--- src/index.js
+++ src/index.js
@@ -5,3 +5,36 @@ if ("serviceWorker" in navigator) {
     console.log("[Info] Service Worker Registered");
   });
 }
+
+// A2HS (add 2 home screen) functionality
+
+// Initialize deferredPrompt for use later to show browser install prompt.
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  showInstallPromotion();
+  // Optionally, send analytics event that PWA install promo was shown.
+  console.log(`[Info] A2HS compatible`);
+
+
+  addBtn.addEventListener("click", () => {
+    // hide our user interface that shows our A2HS button
+    addBtn.style.display = "none";
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the A2HS prompt");
+      } else {
+        console.log("User dismissed the A2HS prompt");
+      }
+      deferredPrompt = null;
+    });
+  });
+});
