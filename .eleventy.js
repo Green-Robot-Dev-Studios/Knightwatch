bbe9a28a1bb6df572c38818fbf0539d2d8eaa832
Nicholas Ficara <nficara01@gmail.com>
2021-10-25 12:00:19 -0400

    October Issue

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,10 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addNunjucksFilter("deslug", (text) => deSlug(text));
   eleventyConfig.addNunjucksFilter("limit", (arr, max) => limit(arr, max));
+  eleventyConfig.addNunjucksFilter("imagesOnly", (arr) => imagesOnly(arr));
+  eleventyConfig.addNunjucksFilter("textOnly", (arr) => textOnly(arr));
+  eleventyConfig.addNunjucksFilter("shuffle", (arr) => shuffle(arr));
+  eleventyConfig.addNunjucksFilter("splitInto", (arr, part) => split(arr, part));
 
   eleventyConfig.addPassthroughCopy("./src/css/");
   eleventyConfig.addWatchTarget("./src/css/");
@@ -17,8 +21,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/*.js");
   eleventyConfig.addWatchTarget("./src/*.js");
 
-  eleventyConfig.addPassthroughCopy("./src/section/**/*.png");
-  eleventyConfig.addWatchTarget("./src/section/**/*.png");
+  eleventyConfig.addPassthroughCopy("./src/section/**/*.jpeg");
+  eleventyConfig.addWatchTarget("./src/section/**/*.jpeg");
 
   eleventyConfig.setUseGitIgnore(false);
 
@@ -38,8 +42,102 @@ function deSlug(text) {
   return text.replaceAll("-", " ");
 }
 
+function split(arr, part) {
+  const part1 = arr.slice(0, Math.floor(arr.length/2))
+  const part2 = arr.slice(Math.floor(arr.length/2))
+
+  if (part === 0) {
+    console.log("Part1")
+    return part1;
+  } else if (part === 1) {
+    console.log("Part2")
+    return part2;
+  } else {
+    return [];
+  }
+}
+
+function shuffleArray(array, seed) {                // <-- ADDED ARGUMENT
+  var m = array.length, t, i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(random(seed) * m--);        // <-- MODIFIED LINE
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+    ++seed                                     // <-- ADDED LINE
+  }
+
+  return array;
+}
+
+function random(seed) {
+  var x = Math.sin(seed++) * 10000; 
+  return x - Math.floor(x);
+}
+
+function shuffle(arr) {
+  console.log(arr[0]["data"]["page"])
+  console.log(arr[0]["data"]["collections"])
+  const seed = new Date().getDate();
+  console.log("SEED", seed)
+  arr = Array.from(arr);
+  console.log(arr.toString())
+  arr = shuffleArray(arr, seed+1);
+  console.log(arr.toString())
+
+  return arr;
+}
+
+function imagesOnly(arr) {
+  arr = arr.filter((doc) => {
+    if (!doc.hasOwnProperty("templateContent")) {
+      console.warn(
+        "[Warning] Failed to extract image: Document has no property `templateContent`."
+      );
+      return false;
+    }
+  
+    const content = doc.templateContent;
+  
+    if (content.includes("<img")) {
+      return true;
+    }
+  
+    return false;
+  });
+
+  return arr;
+}
+
+function textOnly(arr) {
+  arr = arr.filter((doc) => {
+    if (!doc.hasOwnProperty("templateContent")) {
+      console.warn(
+        "[Warning] Failed to extract image: Document has no property `templateContent`."
+      );
+      return false;
+    }
+  
+    const content = doc.templateContent;
+  
+    if (!content.includes("<img")) {
+      return true;
+    }
+  
+    return false;
+  });
+
+  return arr;
+}
+
 function extractFirstImage(doc) {
-  if (!doc.hasOwnProperty("templateContent")) {
+  if (doc && !doc.hasOwnProperty("templateContent")) {
     console.warn(
       "[Warning] Failed to extract image: Document has no property `templateContent`."
     );
@@ -51,7 +149,7 @@ function extractFirstImage(doc) {
   if (content.includes("<img")) {
     const imgTagBegin = content.indexOf("<img");
     const imgTagEnd = content.indexOf(">", imgTagBegin);
-
+    console.log(content.substring(imgTagBegin, imgTagEnd + 1))
     return content.substring(imgTagBegin, imgTagEnd + 1);
   }
 
@@ -59,7 +157,7 @@ function extractFirstImage(doc) {
 }
 
 function extractBlurb(doc) {
-  if (!doc.hasOwnProperty("templateContent")) {
+  if (doc && !doc.hasOwnProperty("templateContent")) {
     console.warn(
       "[Warning] Failed to extract image: Document has no property `templateContent`."
     );
@@ -67,9 +165,24 @@ function extractBlurb(doc) {
   }
 
   const content = doc.templateContent;
+  //console.log(doc)
+  let position = 0;
+  let blurbs = [];
+  while (true) {
+    // no more paragraphs
+    if (content.indexOf("<p>", position) === -1) {
+      // console.log(blurbs)
+      return blurbs.join("<br>").split(" ").slice(0, 100).join(" ") + " ...";
+    } 
 
-  const pTagBegin = content.indexOf("<p>")+3;
-  const pTagEnd = content.indexOf("</p>");
+    const pTagBegin = content.indexOf("<p>", position);
+    const pTagEnd = content.indexOf("</p>", position);
+    position = pTagEnd + 1;
+    
+    if (content.substring(pTagBegin + 3, pTagEnd).includes("<img")) continue;
 
-  return content.substring(pTagBegin, pTagEnd).split(" ").slice(0, 100).join(" ");
+    var regex = /(<([^>]+)>)/ig;
+
+    blurbs.push(content.substring(pTagBegin + 3, pTagEnd).replace(regex, ""));
+  }
 }
--- convert.mjs
+++ convert.mjs
@@ -2,166 +2,222 @@ import { readdir, writeFile, rename, rm, access } from "fs/promises";
 import extract from "extract-zip";
 import path from "path";
 import mammoth from "mammoth";
+import sharp from "sharp";
 import slugify from "slugify";
-import unusedFilename from "unused-filename"
+import unusedFilename from "unused-filename";
 
 // helper function that gets the header data from the .docx
 function getParameter(text, parameter) {
-  const parameterStart = text.indexOf(parameter) + parameter.length;
-  const parameterEnd = text.indexOf("<", parameterStart);
-  const value = text.substring(parameterStart, parameterEnd).trim();
-  if (value === "") throw "Incorrect Header";
-  return value;
+    const parameterStart = text.indexOf(parameter) + parameter.length;
+    const parameterEnd = text.indexOf("<", parameterStart);
+    const value = text.substring(parameterStart, parameterEnd).trim();
+    if (value === "") throw "Incorrect Header";
+    if (parameter !== "Date:") {
+        return '"' + value + '"';
+    } else {
+        return value;
+    }
+}
+
+// why does JS not have this
+function allIndexOf(str, toSearch) {
+    var indices = [];
+    for (
+        var pos = str.indexOf(toSearch);
+        pos !== -1;
+        pos = str.indexOf(toSearch, pos + 1)
+    ) {
+        indices.push(pos);
+    }
+    return indices;
 }
 
 // conversion function
 async function docsToHTML(source, docName) {
-  console.log("[Info] Converting " + source);
-  const options = {
-    styleMap: ["p[style-name='Title'] => h1:fresh"],
-    convertImage: mammoth.images.imgElement(async function (element) {
-      const imageBuffer = await element.read();
-      const directory = source.substring(0, source.lastIndexOf("/"));
-      const imageLocation = await unusedFilename(directory + "/" + docName.replace(".docx", ".png"));
+    console.log("[Info] Converting " + source);
+    const options = {
+        styleMap: ["p[style-name='Title'] => h1:fresh"],
+        convertImage: mammoth.images.imgElement(async function (element) {
+            const imageBuffer = await element.read();
+            const directory = source.substring(0, source.lastIndexOf("/"));
+            const imageLocation = await unusedFilename(
+                directory + "/" + docName.replace(".docx", ".jpeg")
+            );
 
-      try {
-        await writeFile(imageLocation, imageBuffer);
-      } catch {
-        throw "Image could not be processed";
-      }
+            const compressedImage = await sharp(imageBuffer)
+                .jpeg({
+                    quality: 60,
+                    compressionLevel: 9,
+                    adaptiveFiltering: true,
+                    force: true,
+                })
+                .toBuffer();
 
-      const imageSrc = imageLocation.replace("./src/", "/").replace("src", "/");
-      console.log(imageSrc);
-      return { src: imageSrc, class: "content-image" };
-    })
-  };
-  const result = await mammoth.convertToHtml({ path: source }, options);
-  for (const msg of result.messages) {
-    console.log("[Warning] " + msg.message);
-  }
-  // process html before writing to file
-  var toWrite = result.value;
-  let title, author, date;
-  try {
-    title = getParameter(toWrite, "Title:");
-    author = getParameter(toWrite, "Author:");
-    date = getParameter(toWrite, "Date:");
-  } catch (e) {
-    console.log("[Error] " + e);
-    throw "Could not complete " + source + " due to: " + e;
-  }
-  const headerEndPosition = toWrite.indexOf("</p>", toWrite.indexOf("---")) + 4;
-  toWrite = toWrite.slice(headerEndPosition);
-  toWrite =
-    `---\nlayout: article.njk\ntitle: ${title}\nauthor: ${author}\ndate: ${date}\n---\n` +
-    toWrite;
+            try {
+                await writeFile(imageLocation, compressedImage);
+            } catch {
+                throw "Image could not be processed";
+            }
 
-  writeFile(source.slice(0, -5) + ".html", toWrite, (err) => {
-    if (err) {
-      console.error("[Error]" + err);
-      return;
+            const imageSrc = imageLocation
+                .replace("./src/", "/")
+                .replace("src/", "/");
+            console.log(imageSrc);
+            return { src: imageSrc, class: "content-image" };
+        }),
+    };
+    const result = await mammoth.convertToHtml({ path: source }, options);
+    for (const msg of result.messages) {
+        console.log("[Warning] " + msg.message);
     }
-    console.log("[Info] Done converting " + source);
-  });
+    // process html before writing to file
+    var toWrite = result.value;
+    let title, author, date;
+    try {
+        title = getParameter(toWrite, "Title:");
+        author = getParameter(toWrite, "Author:");
+        date = getParameter(toWrite, "Date:");
+    } catch (e) {
+        console.log("[Error] " + e);
+        throw "Could not complete " + source + " due to: " + e;
+    }
+    const headerEndPosition =
+        toWrite.indexOf("</p>", toWrite.indexOf("---")) + 4;
+    toWrite = toWrite.slice(headerEndPosition);
+    toWrite =
+        `---\nlayout: article.njk\ntitle: ${title}\nauthor: ${author}\ndate: ${date}\n---\n` +
+        toWrite;
+
+    let count = 0;
+    while (true) {
+        count++;
+        const driveLinks = `<a href="https://drive.google.com/open?id=`;
+        const position = toWrite.indexOf(driveLinks);
+        if (position === -1 || count > 10) {
+            break;
+        }
+        const id = toWrite.substr(position + driveLinks.length, 33);
+        console.log("ID", id);
+        const endPos = toWrite.indexOf("</a>", position);
+        const firstHalf = toWrite.substring(0, position);
+        const secondHalf = toWrite.substring(endPos + 4);
+        toWrite = `${firstHalf}<iframe src="https://drive.google.com/file/d/${id}/preview" width="640" height="480" allow="autoplay"></iframe>${secondHalf}`;
+    }
+
+    writeFile(source.slice(0, -5) + ".html", toWrite, (err) => {
+        if (err) {
+            console.error("[Error]" + err);
+            return;
+        }
+        console.log("[Info] Done converting " + source);
+    });
 }
 
 // adds metadata to each generated HTML file
 async function makeCollection(location, name) {
-  var toWrite = `{"layouts": "base.njk", "tags": "${name}"}`;
-  writeFile(location, toWrite, (err) => {
-    if (err) {
-      console.error("[Error]" + err);
-      return;
-    }
-    console.log("[Info] Done metadata " + source);
-  });
+    var toWrite = `{"layouts": "base.njk", "tags": "${name}"}`;
+    writeFile(location, toWrite, (err) => {
+        if (err) {
+            console.error("[Error]" + err);
+            return;
+        }
+        console.log("[Info] Done metadata " + source);
+    });
 }
 
 // converts all .docx files to .html and calls makeCollection()
 async function convert() {
-  try {
-    const files = await readdir("./src/section", { withFileTypes: true });
-    for (const file of files) {
-      if (file.isDirectory()) {
-        const pages = await readdir("./src/section/" + file.name);
-        for (const page of pages) {
-          if (page.endsWith(".docx")) {
-            await docsToHTML("./src/section/" + file.name + "/" + page, page);
-          }
+    try {
+        const files = await readdir("./src/section", { withFileTypes: true });
+        for (const file of files) {
+            if (file.isDirectory()) {
+                const pages = await readdir("./src/section/" + file.name);
+                for (const page of pages) {
+                    if (page.endsWith(".docx")) {
+                        await docsToHTML(
+                            "./src/section/" + file.name + "/" + page,
+                            page
+                        );
+                    }
+                }
+                makeCollection(
+                    "./src/section/" + file.name + "/" + file.name + ".json",
+                    file.name
+                );
+            }
         }
-        makeCollection(
-          "./src/section/" + file.name + "/" + file.name + ".json",
-          file.name
-        );
-      }
+    } catch (e) {
+        console.error("[Error] " + e);
     }
-  } catch (e) {
-    console.error("[Error] " + e);
-  }
 }
 
 // extracts zip file to ./src/section/ and calls convert
 async function extractZip(source) {
-  console.log("[Info] Extracting " + source);
-  try {
-    await extract(source, { dir: path.resolve("./src/") });
-    console.log("[Info] Extraction complete");
-
-    // rename Knightwatch to section
-    var exists;
+    console.log("[Info] Extracting " + source);
     try {
-      await access("./src/section");
-      exists = true;
-    } catch {
-      exists = false;
-    }
-    if (exists) await rm("./src/section", { recursive: true });
-    await rename("./src/Knightwatch/", "./src/section/");
+        await extract(source, { dir: path.resolve("./src/") });
+        console.log("[Info] Extraction complete");
 
-    // slugify all filenames
-    const files = await readdir("./src/section", { withFileTypes: true });
-    for (const file of files) {
-      if (file.isDirectory()) {
-        const slug = slugify(file.name, { lower: true });
-        await rename("./src/section/" + file.name, "./src/section/" + slug);
-
-        const subFiles = await readdir("./src/section/" + slug, {
-          withFileTypes: true,
-        });
-        for (const subFile of subFiles) {
-          const base = "./src/section/" + slug + "/";
-          rename(
-            base + subFile.name,
-            base + slugify(subFile.name, { lower: true })
-          );
+        // rename Knightwatch to section
+        var exists;
+        try {
+            await access("./src/section");
+            exists = true;
+        } catch {
+            exists = false;
         }
-      }
-    }
+        if (exists) await rm("./src/section", { recursive: true });
+        await rename("./src/Knightwatch Content/", "./src/section/");
 
-    console.log("[Info] Renamed the content directory.");
-    convert();
-  } catch (e) {
-    console.log("[Error] " + e);
-  }
+        // slugify all filenames
+        const files = await readdir("./src/section", { withFileTypes: true });
+        for (const file of files) {
+            if (file.isDirectory()) {
+                const slug = slugify(file.name, { lower: true });
+                await rename(
+                    "./src/section/" + file.name,
+                    "./src/section/" + slug
+                );
+
+                const subFiles = await readdir("./src/section/" + slug, {
+                    withFileTypes: true,
+                });
+                for (const subFile of subFiles) {
+                    const base = "./src/section/" + slug + "/";
+                    rename(
+                        base + subFile.name,
+                        base + slugify(subFile.name, { lower: true })
+                    );
+                }
+            }
+        }
+
+        console.log("[Info] Renamed the content directory.");
+        convert();
+    } catch (e) {
+        console.log("[Error] " + e);
+    }
 }
 
 // gets the path to the zip file and calls extractZip()
 async function init() {
-  var toUnzip = "";
-  try {
-    const files = await readdir(".");
-    for (const file of files) {
-      if (file.startsWith("Knightwatch") && file.endsWith(".zip")) {
-        toUnzip = file;
-      }
+    var toUnzip = "";
+    try {
+        const files = await readdir(".");
+        for (const file of files) {
+            if (file.startsWith("Knightwatch") && file.endsWith(".zip")) {
+                toUnzip = file;
+            }
+        }
+        if (toUnzip === "") {
+            console.log(
+                "[Error] Please place the google drive zip in this folder."
+            );
+        } else {
+            extractZip("./" + toUnzip);
+        }
+    } catch (err) {
+        console.error(err);
     }
-    if (toUnzip === "") {
-      console.log("[Error] Please place the google drive zip in this folder.");
-    } else {
-      extractZip("./" + toUnzip);
-    }
-  } catch (err) {
-    console.error(err);
-  }
 }
 init();
