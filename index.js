0d8648c9e7896d2f544d9c44e914959f3605f70f
Finn Shadow <finnandshadow@gmail.com>
2023-03-09 08:13:17 -0500

    Updates

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ window.addEventListener('beforeinstallprompt', (e) => {
 
 // Control the timer
 // TODO: Fix bug
-const SUBMISSION_DATE = new Date(2023, 3 - 1, 24, 23, 59);
+const SUBMISSION_DATE = new Date(2023, 3 - 1, 27, 23, 59);
 // const SUBMISSION_DATE = new Date("2022-11-25");
 // const SUBMISSION_DATE = Date.
 // const SUBMISSION_DATE = new Date("2022-11-2");
